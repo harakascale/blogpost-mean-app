0d6901446efb29a32146a1f4307b0ff99766460d
Greg <harakascale@gmail.com>
2021-10-27 12:26:46 -0400

    first commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Post } from '../post.model'
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddPost() {
+    const post: Post ={
+      title: this.enteredTitle,
+      content: this.enteredContent
+    }
+    this.postCreated.emit(post);
+  }
+}
