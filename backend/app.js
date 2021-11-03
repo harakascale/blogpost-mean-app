12be133404712b15d207cb427809a433b7322da2
Greg <harakascale@gmail.com>
2021-11-03 12:01:43 -0400

    worked on fetching, deleting data on backend

--- backend/app.js
+++ backend/app.js
@@ -35,30 +35,30 @@ app.post("/api/posts", (req, res, next) =>{
     title: req.body.title,
     content: req.body.content
   });
-  posts.save();
-  res.status(201).json({
-    message: 'Post added successfully'
+  posts.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    {
-       id: '12344343',
-       title: 'First server-side post',
-       content: 'This is coming from the server'
-    },
-    {
-      id: 'ejwej2323',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'
-   }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+    Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
+
+app.delete("/api/posts/:id", (req, res, next)=>{
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post deleted!' });
+  });
+});
 
 module.exports = app;
