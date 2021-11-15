155ff804c8a6e8723d51d45a6b4b476854c6bbd6
Greg <harakascale@gmail.com>
2021-11-15 17:55:41 -0500

    Implemented SPA Authentication on backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt"); // install bcrypt package - npm install --save bcrypt
-const  User = require("../models/user")
-
+const  User = require("../models/user");
+const jwt = require("jsonwebtoken"); // install jsonwebtoken package - npm install --save jsonwebtoken
+const user = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
@@ -23,6 +24,38 @@ router.post("/signup", (req, res, next) => {
             error: err
           })
         })
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .then(user => {
+      if(!user){
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result){
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign({
+        email: user.email, userId: user._id},
+        'secret_this_should_be_longer',
+        { expiresIn: '1h'}
+      );
+      res.status(200).json({
+        token: token
+      })
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
 });
 
