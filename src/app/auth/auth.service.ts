950c190f66e034fbc6da627b06054a474cbdb82c
Greg <harakascale@gmail.com>
2021-11-17 12:21:35 -0500

    reflect token expiration in the UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { AuthData } from "./auth-date.model";
 
@@ -9,9 +10,10 @@ import { AuthData } from "./auth-date.model";
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router: Router){}
 
   createUser(email: string, password: string){
     const authData: AuthData = { email: email, password: password};
@@ -35,13 +37,19 @@ export class AuthService {
 
   login(email: string, password: string){
     const authData: AuthData = { email: email, password: password};
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if(token){
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() =>{
+            this.logout();
+          }, expiresInDuration * 1000); //setTimeout works with millseconds and not seconds therfore * 1000
+          console.log(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
       });
   }
@@ -50,5 +58,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
+    this.router.navigate(['/']);
   }
 }
