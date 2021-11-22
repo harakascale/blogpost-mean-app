5ea261e8f12a6063dce9eb684e2c3594840881cc
Greg <harakascale@gmail.com>
2021-11-22 17:06:58 -0500

    added more environment variables

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const userRoutes  = require("./routes/user");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://gwhyte2021:pJdvDA4jSBWZzNol@cluster0.ipz87.mongodb.net/node-angular?retryWrites=true&w=majority")
+mongoose.connect(
+  "mongodb+srv://gwhyte2021:"+ process.env.MONGO_ATLAS_PW +"@cluster0.ipz87.mongodb.net/node-angular?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to database')
   })
